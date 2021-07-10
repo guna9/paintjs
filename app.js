9fc261bce02dd46cc2f92637a56c4a8309d1fd8c
guna9 <osh01039@naver.com>
2021-07-10 16:16:13 +0900

    캔버스 채우기와 toDataURL로 SAVE까지

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 600;
-canvas.height = 600;  //캔버스의 사이즈 알려주기
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 600;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;  //캔버스의 사이즈 알려주기
+
+ctx.fillStyle = "white"; //캔버스배경색 설정
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;  //defalut값 false
 
 function stopPainting(){
     painting = false;
@@ -29,16 +40,64 @@ function onMouseMove(event){
     }  //마우스가 캔버스 안에 들어갔을때 마우스의 위치를 따라다니며(path) 그릴 준비 하고, 마우스 클릭이 되었을 때 그림을 그림
 }
 
-function onMouseDown(event){
-    painting = true;
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
 
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
 
+function handleCanvasClick(){
+    if (filling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+function handleCM(event){
+    event.preventDefault();  //마우스 우클릭방지
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image; 
+    link.download = "PaintJS[💕]"; 
+    link.click();
+} // <a download></a>와 같은 역할
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));  //forEach뒤에 color => color를 potato로 바꿔도 됨 /네이밍
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
 }
